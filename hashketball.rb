1706acc8c27fc4bf7e32876c09adaef4b67bd584
Shreya Sridhar <shreyamohak@outlook.com>
2020-11-13 23:43:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -136,9 +136,10 @@ def shoe_size(p_name)
   game.each do |team, team_hash|
     team_hash[:players].each do |player_hash|
       if player_hash[:player_name] == p_name
-        player_hash[:shoe]
+        k=player_hash[:shoe]
       end
     end
   end
+  k
 end
 
