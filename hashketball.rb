722971f6b17a4824f6030c314f72790ef5ea0436
Shreya Sridhar <shreyamohak@outlook.com>
2020-11-13 23:39:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,14 +127,7 @@ def game_hash
 end
 
 def num_points_scored(player_name)
-  game = game_hash()
-    game.each do |team, team_hash|
-    team_hash[:players].each do |player, player_hash|
-      if player_hash[:name] == player_name
-        return player_hash[points]
-      end
-    end
-  end
+
 end
 
 # Write code here
