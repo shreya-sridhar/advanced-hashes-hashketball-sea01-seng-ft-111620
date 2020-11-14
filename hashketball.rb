d6c63ebf2f3e37c887530bd8c93b8ab8fcf7af8b
Shreya Sridhar <shreyamohak@outlook.com>
2020-11-14 00:03:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -202,7 +202,9 @@ def big_shoe_rebounds
   game = game_hash()
   game.each do |team, team_hash|
   team_hash[:players].each do |player_hash|
-    
+    if player_hash[:shoe] > max_shoe_size :
+      max_shoe_size = player_hash[:shoe]
+      rebound_number = player_hash[]
 end 
 
 
